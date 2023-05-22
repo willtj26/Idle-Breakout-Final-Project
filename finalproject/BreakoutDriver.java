438ab98cd89a86e6420f48079b95c88d49d0efea
WILLIAM DEPEW <william@depewhouse.com>
2023-05-22 07:52:58 -0400

    blah

--- finalproject/BreakoutDriver.java
+++ finalproject/BreakoutDriver.java
@@ -2,7 +2,7 @@ import javax.swing.*;
 
 public class BreakoutDriver {
    public static void main(String[] args) { 
-      JFrame frame = new JFrame("Panel Swapping");
+      JFrame frame = new JFrame("IDLE BREAKOUT!");
       frame.setSize(600, 430);
       frame.setLocation(20, 20);
       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
